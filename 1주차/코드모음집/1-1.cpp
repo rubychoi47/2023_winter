bf0e21729a3764f2cc949e091d6552202858120e
rubychoi47 <135409143+rubychoi47@users.noreply.github.com>
2024-01-08 16:48:44 +0900

    Add files via upload

---
+++ 1주차/코드모음집/1-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#define TEN 10
+
+using namespace std;
+int main()
+{
+	int x;
+	x=50;
+	cout <<"Let's study C++ language."<<"\n";
+	cout << "TEN=" << TEN <<"\n";
+	cout << "x=" << x <<"\n";
+	return 0;
+}
---
+++ 1주차/코드모음집/1-2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello.\n"; // cout << "Hello. " << "\n"ÀÌ¶û °°À½. 
+	cout <<12345 << "\n";
+	return 0;
+}
